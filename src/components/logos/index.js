fd79ef476536e50071b8d89f42828299871f821f
Lima404 <gabriel.lima2307@uol.com.br>
2024-02-23 12:05:05 -0300

    add styled-components logo

--- src/components/logos/index.js
+++ src/components/logos/index.js
@@ -1,12 +1,20 @@
-import './logos.css'
+//import './logos.css'
 import logo from '../../imagens/logo.svg'
+import styled from 'styled-components'
+
+const LogoContainer = styled.div`
+    display: flex;
+    font-size: 30px;
+    gap: 0px 10px;
+`
+
 
 const Logos = () => {
     return(
-        <div className='logo'>
+        <LogoContainer>
             <img src={logo} alt='logo'></img>
             <p><strong>Alura</strong>Books</p>
-          </div>
+        </LogoContainer>
     )
 }
 
