22640df4bb00c12eda82e5f7832454e4c7db71ef
Lima404 <gabriel.lima2307@uol.com.br>
2024-02-23 14:27:43 -0300

    create new file component pesquisa

---
+++ src/components/pesquisa/dadosPesquisa.js
@@ -0,0 +1,24 @@
+import livro from '../../imagens/livro.png'
+
+export const livros = [
+    {
+        nome: 'Liderança em Design',
+        id: 1,
+        src: livro
+    },
+    {
+        nome: 'Javascript Assertivo',
+        id: 2,
+        src: livro
+    },
+    {
+        nome: 'Cangaceiro Javascript',
+        id: 3,
+        src: livro
+    },
+    {
+        nome: 'Aventureiros Javascript',
+        id: 4,
+        src: livro
+    },
+]
