bc306930d4cad4368e0460f8160910d8c89584e9
Lima404 <gabriel.lima2307@uol.com.br>
2024-02-23 12:04:45 -0300

    add styled-components icones

--- src/components/iconesheader/index.js
+++ src/components/iconesheader/index.js
@@ -1,6 +1,17 @@
-import './icones.css'
 import perfil from '../../imagens/perfil.svg'
 import sacola from '../../imagens/sacola.svg'
+import styled from 'styled-components'
+
+const IconesHeader = styled.ul`
+  display: flex;
+  align-items: center;
+`
+
+const IconeHeader = styled.li`
+  margin-right: 40px;
+  width: 25px;
+
+`
 
 const icones = [
     perfil,
@@ -9,11 +20,11 @@ const icones = [
 
 const Icones = () => {
     return(
-        <ul className='icones'>
+        <IconesHeader>
             { icones.map( (icone) => (
-              <li className='icone'><img src={icone}></img></li>
+              <IconeHeader><img src={icone}></img></IconeHeader>
             )) }
-        </ul>
+        </IconesHeader>
     )
 }
 
