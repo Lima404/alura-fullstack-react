7aa4473d3a337f3bb2f8b6f1ce6c5ba5c3de5da5
Lima404 <gabriel.lima2307@uol.com.br>
2024-02-27 16:06:18 -0300

    correção de rotas

--- src/servicos/favoritos.js
+++ src/servicos/favoritos.js
@@ -1,14 +1,26 @@
 import axios from "axios"
 
-const favoritosAPI = axios.create({baseURL: "http://localhost:8000/favoritos"})
+const favoritosAPI = axios.create({baseURL: "http://localhost:8000/favoritos", headers:{"Content-Type":"application/json"}})
 
 async function getFavoritos() {
-        const response = await favoritosAPI.get('/')
+    const response = await favoritosAPI.get('/')
 
-        return response.data
+    return response.data
+}
 
+async function postFavorito(id, nome) {
+    const objetocriado = {id, nome}
+    console.log(objetocriado)
+
+    await axios.post(`http://localhost:8000/favoritos`, objetocriado)
+}
+
+async function deleteFavorito(id) {
+    await favoritosAPI.delete(`/${id}`)
 }
 
 export {
-        getFavoritos
+    getFavoritos,
+    postFavorito,
+    deleteFavorito
 }
